e813ffc5d92ea8e1f4112029eb2ec4fcf77fbe39
Barber-Studio <ciaociao10101011@gmail.com>
2026-09-03 03:46:17 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -8,10 +8,7 @@
 // CONFIGURAZIONE SUPABASE
 // ==========================================
 
-const SUPABASE_URL = "https://wxcdmtajcasnlohqkgmk.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_bwjP-ihASijevvu7d6r5Ew_6JaWKSDP";
 
-);
 
 
 // ==========================================
