1d4f4066b0a84abc64fd96fa308354cd1192aa19
Barber-Studio <ciaociao10101011@gmail.com>
2026-09-03 03:24:38 +0200

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,6 @@
-const SUPABASE_URL = https://wxcdmtajcasnlohqkgmk.supabase.co;
+const SUPABASE_URL = "https://wxcdmtajcasnlohqkgmk.supabase.co";
 
-const SUPABASE_ANON_KEY = sb_publishable_bwjP-ihASijevvu7d6r5Ew_6JaWKSDP;
+const SUPABASE_ANON_KEY = "sb_publishable_bwjP-ihASijevvu7d6r5Ew_6JaWKSDP";
 
 const supabaseClient = supabase.createClient(
   SUPABASE_URL,
