8c00ba056728f5e599c3fde7e1a3bb321ee0d5ad
Barber-Studio <ciaociao10101011@gmail.com>
2026-09-05 00:25:41 +0200

    Delete js/supabase.js

--- js/supabase.js
+++
@@ -1,8 +0,0 @@
-const SUPABASE_URL = "https://wxcdmtajcasnlohqkgmk.supabase.co";
-
-const SUPABASE_ANON_KEY = "sb_publishable_bwjP-ihASijevvu7d6r5Ew_6JaWKSDP";
-
-const supabaseClient = supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY
-);
